a3dce13776bfb3b7b740f8565eaee92b84e380ec
llasantinelli <ich.lauri@gmail.com>
2024-07-22 18:20:01 -0300

    Classical Introductory Exercise

---
+++ solutions/hello-world/src/test/java/GreeterTest.java
@@ -0,0 +1,35 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class GreeterTest {
+
+  @Test
+  public void testThatGreeterReturnsTheCorrectGreeting() {
+    assertThat(new Greeter().getGreeting()).isEqualTo("Hello, World!");
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
